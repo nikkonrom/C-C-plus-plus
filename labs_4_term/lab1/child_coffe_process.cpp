7f6710b9e4aacd8264b466429d8f61068474c6f1
Никита Романюк <nikkonrom@gmail.com>
2018-02-26 04:09:29 +0300

    Added the fisrt system software laboratory work in initial form

---
+++ labs_4_term/lab1/child_coffe_process.cpp
@@ -0,0 +1,33 @@
+#include <cstdio>
+#include <cstdlib>
+#include <Windows.h>
+#include <conio.h>
+#include <string>
+#include <iostream>
+
+enum PriceList
+{
+	Espresso = 100,
+	Ñappuccino = 150,
+	Latte = 200,
+	FlatWhite = 250,
+	HoneyRaft = 300
+};
+
+int main()
+{
+	int balance = 0; //amount of stored currensy (in pennies)
+	
+	while(true)
+	{
+		std::string message;
+		std::cin >> message;
+
+	}
+
+
+	return 0;
+}
+
+
+
